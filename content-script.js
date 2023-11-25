5195815c332a4cfc5e38777b2acc9208da164311
Francois Robbertze <67432234+francoisrob@users.noreply.github.com>
2023-11-25 19:15:07 +0200

    fix: changes

--- content-script.js
+++ content-script.js
@@ -1,10 +1,5 @@
-const API_URL = "http://localhost:3000/api/assist";
-// const API_URL = "https://zinq.francoisrob.me/api/assist";
-let key;
-
-chrome.storage.sync.get(["key"], function (result) {
-  key = result.key;
-});
+// const API_URL = "http://localhost:3000/api/assist";
+const API_URL = "https://zinq.francoisrob.me/api/assist";
 
 function attachButton() {
   const section = document.getElementsByClassName("appHeaderWrapper___uyPti");
@@ -36,100 +31,101 @@ function attachButton() {
   tearOption.value = "tear";
   select.add(tearOption);
 
-  button.addEventListener("click", () => {
-    if (!key) {
-      const newKey = prompt("Please enter your TornStats API key");
-      if (newKey) {
-        chrome.storage.sync.set({ key: newKey }, function () {
-          console.log("Key saved");
-        });
-        key = newKey;
-      }
+  // const removeApiKeyButton = document.createElement("button");
+  // removeApiKeyButton.innerHTML = "Remove API Key";
+  // removeApiKeyButton.id = "remove-api-key-button";
+  // removeApiKeyButton.className = `torn-btn red`;
+  // removeApiKeyButton.style.cursor = "pointer";
+  // removeApiKeyButton.style.color = "const(--default-red-color)";
+  // removeApiKeyButton.style.marginLeft = "10px";
+
+  // removeApiKeyButton.addEventListener("click", async () => {
+  //   chrome.storage.sync.remove(["key"], () => {
+  //     alert("API Key removed. Please refresh the page.");
+  //   });
+  // });
+
+  button.addEventListener("click", async () => {
+    const url = getUrl();
+    const attackerId = url.split("ID=")[1];
+
+    const stats = await getTornStatsData(attackerId);
+    if (!stats || stats.status === false) {
+      console.log('TornStats not found.');
     }
-    clickButton();
+
+    const data = getData();
+    if (!data) {
+      alert("Attack data not found. Please try again.");
+      return;
+    }
+    const json = JSON.parse(data);
+
+    if (json.DB?.error) {
+      alert(json.DB.error);
+      return;
+    }
+
+    if (
+      !json.DB?.attackStatus === "process" ||
+      json.DB?.attackStatus === "notStarted" ||
+      json.DB?.attackStatus === "nonAttack"
+    ) {
+      alert("You have to start a fight first.");
+      return;
+    }
+
+    const tornID = json.DB?.attackerUser?.userID || url.split("user2ID=")[1];
+    if (!tornID) {
+      alert("You have to start a fight first.");
+      return;
+    }
+
+    const select = document.getElementById("assist-select");
+    const assistType = select.value;
+    const body = { tornID, url, assistType, json, stats };
+    sendPostRequest(API_URL, body);
   });
 
   section[0].appendChild(button);
   section[0].appendChild(select);
-}
-
-async function clickButton() {
-  const url = getUrl();
-  const attackerId = url.split("ID=")[1];
-  const data = getData();
-  const stats = await getTornStatsData(attackerId);
-  if (!data) {
-    alert("Attack data not found. Please try again.");
-    return;
-  }
-  const json = JSON.parse(data);
-
-  if (stats.status) {
-    alert(stats.message);
-    return;
-  }
-
-  if (json.DB?.error) {
-    alert(json.DB.error);
-    return;
-  }
-
-  if (!json.DB?.attackStatus === 'process' || json.DB?.attackStatus === 'notStarted' || json.DB?.attackStatus === 'nonAttack') {
-    alert("You have to start a fight first.");
-    return;
-  }
-  const tornID = json.DB?.attackerUser?.userID || url.split("user2ID=")[1];
-  if (!tornID) {
-    alert("You have to start a fight first.");
-    return;
-  }
-
-  const select = document.getElementById("assist-select");
-  const assistType = select.options[select.selectedIndex].value;
-  const body = {
-    tornID: tornID,
-    url: url,
-    assistType: assistType,
-    json: json,
-    stats: stats
-  };
-  sendPostRequest(API_URL, body);
-}
-
-function getTornID() {
-  const headerData = sessionStorage.getItem("headerData");
-  const tornID = JSON.parse(headerData).user.data.userID;
-  return tornID;
+  section[0].appendChild(removeApiKeyButton);
 }
 
 function getUrl() {
-  const url = window.location.href;
-  return url;
-}
-
-function sendPostRequest(url, data) {
-  const xhr = new XMLHttpRequest();
-  xhr.open("POST", url, true);
-  xhr.setRequestHeader("Content-Type", "application/json");
-  try {
-    xhr.send(JSON.stringify(data));
-    console.log("Sent!");
-  } catch (e) {
-    alert(e);
-  }
+  return window.location.href;
 }
 
 function getData() {
-  const assistScriptDataContainer = document.querySelector('#assist-script-data-container');
-  return assistScriptDataContainer?.textContent
+  const assistScriptDataContainer = document.getElementById("attackData");
+  return assistScriptDataContainer?.textContent;
 }
 
 async function getTornStatsData(tornID) {
-  const url = `https://www.tornstats.com/api/v2/${key}/spy/user/${tornID}`
-  const response = await fetch(url);
+  if (!key) {
+    console.log("API Key not found.");
+    return;
+  }
+  const response = await fetch(
+    `https://www.tornstats.com/api/v2/${key}/spy/user/${tornID}`
+  );
   const json = await response.json();
   return json;
 }
 
-attachButton();
+function sendPostRequest(url, data) {
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then(() => console.log("Sent!"))
+    .catch((e) => alert(e));
+}
 
+chrome.storage.sync.get(["key"], function (result) {
+  key = result.key;
+  attachButton();
+});
